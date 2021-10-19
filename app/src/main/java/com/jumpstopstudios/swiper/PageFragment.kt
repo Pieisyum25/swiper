aa40e8607115edcee7e73edfd14a27227ad7b9e2
John Flynn <johnhfflynn25@gmail.com>
2021-10-19 15:42:24 +1300

    Added ViewPager2, now fully functional
    ViewPager2 now successfully allows swiping between different pages/cards. I will merge to main before adding new features like animations.

--- app/src/main/java/com/jumpstopstudios/swiper/PageFragment.kt
+++ app/src/main/java/com/jumpstopstudios/swiper/PageFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class PageFragment : Fragment() {
+class PageFragment(private val index: Int) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_page, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.page_title).text = getString(R.string.page_title, index)
+    }
 }
